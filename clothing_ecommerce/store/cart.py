d82d1df8d454bce42d0785e18dadf19c68078c6d
BigyanParajuli <119508293+BigyanParajuli@users.noreply.github.com>
2024-05-06 22:21:37 +0545

    imporoved card increase and decrease

--- clothing_ecommerce/store/cart.py
+++ clothing_ecommerce/store/cart.py
@@ -36,11 +36,14 @@ class Cart(object):
         
         if update_quantity:
             self.cart[product_id]['quantity'] += int(quantity)
+            
+            if self.cart[product_id]['quantity'] == 0:
+                self.remove(product_id)
         
         self.save()
     
     def remove(self, product_id):
-        if str(product_id) in self.cart:
+        if product_id in self.cart:
             del self.cart[product_id]
 
             self.save()
@@ -49,4 +52,4 @@ class Cart(object):
         for p in self.cart.keys():
             self.cart[str(p)]['product'] = Product.objects.get(pk=p)
         
-        return int(sum(item['product'].price * item['quantity'] for item in self.cart.values())) 
+        return int(sum(item['product'].price * item['quantity'] for item in self.cart.values())) / 100
